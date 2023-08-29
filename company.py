f525d099877577e180ed271a0cb116760798e96c
Eric Tao <erictao04@gmail.com>
2023-08-29 18:33:41 -0400

    Extract employees from company

--- company.py
+++ company.py
@@ -2,6 +2,7 @@ import csv
 
 from collections import namedtuple
 from email_builder import EmailBuilder
+from employee import Employees
 from template import Template
 from utils import get_employee_path
 from draft import Draft
@@ -12,26 +13,11 @@ class Company:
         self.company = company.lower()
         self.email_builder = email_builder
         self.template = Template(self.company)
-        self.employees = self.read_names()
+        self.employees = Employees(self.company)
         self.draft = Draft(self.company)
 
-    def read_names(self):
-        Employee = namedtuple('Employee', ('first_name', 'last_name'))
-
-        employee_path = get_employee_path(self.company)
-        employees = []
-
-        with open(employee_path) as csv_file:
-            csv_reader = csv.reader(csv_file)
-
-            for row in csv_reader:
-                first_name, last_name = [name.lower() for name in row]
-                employees.append(Employee(first_name, last_name))
-
-        return employees
-
     def create_drafts(self):
-        for first_name, last_name in self.employees:
+        for first_name, last_name in self.employees.names:
             email = self.email_builder.build_email(first_name, last_name)
             subject = self.template.subject
             body = self.template.update(first_name)
---
+++ employee.py
@@ -0,0 +1,26 @@
+from collections import namedtuple
+import csv
+
+from utils import get_employee_path
+
+
+Name = namedtuple('Name', ('first_name', 'last_name'))
+
+
+class Employees:
+    def __init__(self, company) -> None:
+        self.company = company
+        self.names = self.get_names()
+
+    def get_names(self):
+        employee_path = get_employee_path(self.company)
+        names = []
+
+        with open(employee_path) as csv_file:
+            csv_reader = csv.reader(csv_file)
+
+            for row in csv_reader:
+                first_name, last_name = [name.lower() for name in row]
+                names.append(Name(first_name, last_name))
+
+        return names
