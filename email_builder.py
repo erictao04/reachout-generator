b46008263981b2ff84b350a7ed70c8432608c7a5
Eric Tao <erictao04@gmail.com>
2023-08-30 13:47:27 -0400

    add email builders and trim employee names

--- email_builder.py
+++ email_builder.py
@@ -17,3 +17,27 @@ class FirstLastEmailBuilder(EmailBuilder):
 
     def build_email(self, first_name: str, last_name: str) -> str:
         return f'{first_name}.{last_name}{self.domain}'
+
+
+class FirstEmailBuilder(EmailBuilder):
+    '''
+    Eric Tao, @splunk.com --> eric@splunk.com
+    '''
+
+    def __init__(self, domain) -> None:
+        self.domain = domain
+
+    def build_email(self, first_name: str, last_name: str) -> str:
+        return f'{first_name}{self.domain}'
+
+
+class FirstInitialLastEmailBuilder(EmailBuilder):
+    '''
+    Eric Tao, @splunk.com --> etao@splunk.com
+    '''
+
+    def __init__(self, domain) -> None:
+        self.domain = domain
+
+    def build_email(self, first_name: str, last_name: str) -> str:
+        return f'{first_name[0]}{last_name}{self.domain}'
--- employee.py
+++ employee.py
@@ -17,9 +17,12 @@ class Employees:
         names = []
 
         with open(employee_path) as csv_file:
-            csv_reader = csv.reader(csv_file)
+            csv_reader = csv.reader(csv_file, delimiter=" ")
 
             for row in csv_reader:
+                if row[-1] == "":
+                    row = row[:-1]  # strip trailing space
+
                 first_name, last_name = [name.lower() for name in row]
                 names.append(Name(first_name, last_name))
 
